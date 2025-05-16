bf1b3b0511112b1db7cd29a67f1976e7135cd84f
kon21 <kon21pan@gmail.com>
2025-05-17 00:54:52 +0300

    sddds

--- services/searchShopsServices.js
+++ services/searchShopsServices.js
@@ -4,12 +4,16 @@ const Shop = require('../models/shop');
 // Αναζήτηση καταστημάτων με βάση την πόλη
 const searchShopsByCityService = async (cityId) => {
   try {
-    const shops = await Shop.find({ city: cityId }).select('_id shopName reviewRatingAverage  images');
+    const shops = await Shop.find({ city: cityId }).select('_id shopName reviewRatingAverage images address location category');
     return shops.map(shop => ({
       id: shop._id,
       shopName: shop.shopName,
       reviewRatingAverage: shop.reviewRatingAverage,
       image: shop.images[0] || null,
+      images: shop.images,
+      address: shop.address,
+      location: shop.location,
+      category: shop.category,
     }));
   } catch (error) {
     throw new Error('Error fetching shops by city: ' + error.message);
@@ -23,6 +27,9 @@ const searchShopsByCityAndRegionService = async (cityId, regionId) => {
     return shops.map(shop => ({
       id: shop._id,
       shopName: shop.shopName,
+      location:shop.location,
+      address:shop.address,
+      category:shop.category,
       reviewRatingAverage: shop.reviewRatingAverage,
       image: shop.images[0] || null,
     }));
@@ -34,13 +41,16 @@ const searchShopsByCityAndRegionService = async (cityId, regionId) => {
 // Αναζήτηση καταστημάτων με βάση την πόλη και την κατηγορία
 const searchShopsByCityAndCategoryService = async (cityId, categoryId) => {
   try {
-    const shops = await Shop.find({ city: cityId, category: categoryId }).select('_id shopName  address reviewRatingAverage images');
+    const shops = await Shop.find({ city: cityId, category: categoryId }).select('_id shopName reviewRatingAverage images address location category');
     return shops.map(shop => ({
       id: shop._id,
       shopName: shop.shopName,
       reviewRatingAverage: shop.reviewRatingAverage,
       image: shop.images[0] || null,
-      address:shop.address,
+      images: shop.images,
+      address: shop.address,
+      location: shop.location,
+      category: shop.category,
     }));
   } catch (error) {
     throw new Error('Error fetching shops by city and category: ' + error.message);
